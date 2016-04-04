44ad8a5658e0594965d2023473253c18d1d76fb5
Gabriel <ghoule3@uw.edu>
2016-04-03 19:12:06 -0700

    Worked on PCB. init and toString not working...

--- PCB.c
+++ PCB.c
@@ -2,37 +2,69 @@
  * Group 3 OS ADTs
  */
 
-#include <stdio.h>
-
 #ifndef ADT
 #define ADT
 #include "PCB.h"
 #endif
 
 PCB_p PCB_construct(void) {
-  PCB_p result = malloc(sizeof(PCB_p));
-
-  return result;
+  PCB_p *result = malloc(sizeof(PCB));
+  return *result;
 }
 
-void PCB_destruct(PCB_P thePCB) {
+void PCB_destruct(PCB_p thePCB) {
   if (thePCB) {
     free(thePCB);
   }
 }
 
-int PCB_init(PCB_p) {
+int PCB_init(PCB_p thePCB) {
 
+  //not sure what values to give.
+
+  thePCB->state = new;
+  thePCB->priority = 15; // lowest priority?
+  thePCB->pid = 0;
+  thePCB->pc = 0;
 }
 
-int PCB_set_pid(PCB_p thePCB, unsigned long pid) {
-  thePCB->pid = pid;
+int PCB_set_pid(PCB_p thePCB, unsigned long thePID) {
+  if (thePID) {
+    thePCB->pid = thePID;
+  }
 }
 
-unsigned long PCB_get_pid(PCB_p) {
-
+unsigned long PCB_get_pid(PCB_p thePCB) {
+  return thePCB->pid;
 }
 
-char * PCB_toString(PCB_p thePCB) {
+void PCB_set_state(PCB_p thePCB, state_type theState) {
+  if (theState) {
+    thePCB->state = theState;
+  }
+}
 
+state_type PCB_get_state(PCB_p thePCB) {
+  return thePCB->state;
+}
+
+void PCB_toString(PCB_p thePCB, char *theStr) {
+  if (theStr) {
+    char state[8];
+
+    if (thePCB->state == new) {
+      strcpy(state, "new");
+    } else if (thePCB->state == ready) {
+      strcpy(state, "ready");
+    } else if (thePCB->state == running) {
+      strcpy(state, "running");
+    } else if (thePCB->state == waiting) {
+      strcpy(state, "waiting");
+    } else if (thePCB->state == halted) {
+      strcpy(state, "halted");
+    }
+
+    sprintf(theStr, "PID: %X, State: %s, Priority: %X, PC: %X\n",
+      thePCB->pid, thePCB->priority, thePCB->pc);
+  }
 }
