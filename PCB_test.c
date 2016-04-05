73b29744aa3fb1138fc52eb2a41a9f4e83bc1e73
ghoule636 <ghoule636@hotmail.com>
2016-04-05 08:35:35 -0700

    Fixed PCB class, added one test

--- PCB_test.c
+++ PCB_test.c
@@ -7,15 +7,19 @@
 int main() {
   PCB_p test = PCB_construct();
   PCB_init(test);
+
   char * testStr = malloc(50);
-  //PCB_toString(test, testStr);
+  PCB_toString(test, testStr);
+  printf("contents: %s", testStr);
 
-  //printf("contents: %s", testStr);
+  PCB_set_state(test, ready);
 
+  PCB_toString(test, testStr);
+
+  printf("contents: %s", testStr);
 
   PCB_destruct(test);
   free(testStr);
-  printf("hello");
 
   return 0;
 }
